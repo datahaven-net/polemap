c3e137c95bdc7e40a7b0981b82f5de46d0c5d181
Veselin Penev <penev.veselin@gmail.com>
2021-09-10 17:11:18 +0200

    built a separate page to display results

--- src/main/urls.py
+++ src/main/urls.py
@@ -9,5 +9,6 @@ handler404 = 'main.views.handler404'
 handler500 = 'main.views.handler500'
 
 urlpatterns = [
+    path('<str:pole_code>', main_views.PoleCodeView.as_view(), name='pole_code'),
     path('', main_views.PoleNumbersView.as_view(), name='pole_numbers'),
 ]
